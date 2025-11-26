d4f098edc96f2100ba435d08b8f987f6225e6513
MaiteALC <maitexp549@gmail.com>
2025-11-26 16:04:10 -0300

    Added some new fields and enums to improved attributes and implemented more validation logics

--- apps/web-api-java/src/main/java/br/com/biosecure/model/product/CultureMedia.java
+++ apps/web-api-java/src/main/java/br/com/biosecure/model/product/CultureMedia.java
@@ -1,38 +1,106 @@
 package br.com.biosecure.model.product;
 
-import br.com.biosecure.model.product.Sanitizer.PhysicalForm;
-
+import java.util.ArrayList;
 import java.time.LocalDate;
 
 public class CultureMedia extends Product {
     private final CultureMediaFinality finality;
     private final StorageConditions storageConditions;
-    private final PhysicalForm form;
-    private final boolean readyToUse;
+    private final PhysicalUnit physicalUnit;
     private final boolean protectOfLight;
+    private final double preparationGramsPerLiter;
+    private final double finalPhLevel;
+    private final double qtdPerUnit;
+    private final QuantificationUnit quantificationUnit;
 
-    public CultureMedia(String name, double price, PhysicalForm form, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, MeasureUnit measureUnit, int qtdPerPackage, CultureMediaFinality finality, boolean isReadyToUse, StorageConditions storageConditions, boolean protectOfLight) {
+    public CultureMedia(String name, double price, PhysicalUnit physicalUnit, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, MeasureUnit measureUnit, int qtdPerPackage, CultureMediaFinality finality, StorageConditions storageConditions, boolean protectOfLight, double quantityPerUnit, QuantificationUnit quantificationUnit, double preparationGramsPerLiter, double finalPhLevel) {
         super(name, price, manufacturer, batchNumber, expirationDate, packagingType, measureUnit, qtdPerPackage);
 
+        if (finalPhLevel < 0 || finalPhLevel > 14) {
+            throw new InvalidProductAttributeException("final pH level");
+        }
+
+        if (physicalUnit.isRTU() != quantificationUnit.isToRTUProduct()) {
+            throw new InvalidProductAttributeException("quantification unit");
+        }
+
+        if (!physicalUnit.isRTU() && preparationGramsPerLiter <= 0) {
+            throw new InvalidProductAttributeException("preparation grams per liter");
+        }
+
+        validateBioSafetyRules(physicalUnit, storageConditions);
+
         this.finality = finality;
-        this.readyToUse = isReadyToUse;
         this.storageConditions = storageConditions;
+        this.physicalUnit = physicalUnit;
         this.protectOfLight = protectOfLight;
-        this.form = form;
+        this.qtdPerUnit = quantityPerUnit;
+        this.quantificationUnit = quantificationUnit;
+        this.preparationGramsPerLiter = preparationGramsPerLiter;
+        this.finalPhLevel = finalPhLevel;
+    }
+
+    private void validateBioSafetyRules(PhysicalUnit format, StorageConditions storageTemp) {
+        ArrayList<String> invalids = new ArrayList<>();
+
+        invalids.add("Storage conditions");
+        invalids.add("Physical unit");
+
+        if (!format.isRTU() && (storageTemp == StorageConditions.AMBIENT_TEMP || storageTemp == StorageConditions.FRESH) ) {
+            throw new BioSecurityException(
+                    "Preparated plates/tubes/bottles requires refrigeration (8°C or less) to don't contaminate or dry out.", invalids
+            );
+        }
+
+        if (format.isRTU() && (storageTemp != StorageConditions.AMBIENT_TEMP && storageTemp != StorageConditions.FRESH) ) {
+            throw new BioSecurityException(
+                    "Dehydrated powder requires ambient temperature to don't compromise effectiveness.", invalids
+            );
+        }
     }
 
     public enum CultureMediaFinality {
-        SELECTIVE,
-        DIFFERENTIAL,
-        TRANSPORT,
-        ENRICHMENT;
+        SELECTIVE("Don't kills some microorganisms (the selects/desirable) and kills the others"),
+        DIFFERENTIAL("Used to differentiate the microorganisms in the sample (via ph indicators or dyes, e.g"),
+        TRANSPORT("Used to conservative the sample, not to stimulate microorganisms grows"),
+        ENRICHMENT("Stimulates the growth of desired microorganisms that use to be in less quantity"),
+        CHROMOGENIC("Uses chromogens to give color to microorganisms that contain certain enzymes"),
+        SIMPLE("For general purposes");
+
+        private final String label;
+
+        CultureMediaFinality(String label) {
+            this.label = label;
+        }
+
+        public String getLabel() {
+            return label;
+        }
+    }
+
+    public enum PhysicalUnit {
+        DEHYDRATED_POWDER_BOTTLE(false),
+        DEHYDRATED_POWDER_SACHET(false),
+        PREPARED_LIQUID_BOTTLE(true),
+        PREPARED_LIQUID_TUBE(true),
+        PREPARED_LIQUID_PLATE(true);
+
+        private final boolean isRTU;
+
+        PhysicalUnit(boolean isRTU) {
+            this.isRTU = isRTU;
+        }
+
+        public boolean isRTU() {
+            return isRTU;
+        }
     }
 
     public enum StorageConditions {
         AMBIENT_TEMP(15, 30, "AMB"),
-        FROZEN(-20, 0, "FRO"),
-        REFRIGERATED(2, 8, "REF"),
         FRESH(8, 15, "FRE"),
+        REFRIGERATED(2, 8, "REF"),
+        FROZEN(-20, 0, "FRO"),
         ULTRA_FREEZER(-150, -20, "ULT");
 
         private final int minTemp;
@@ -58,6 +126,23 @@ public class CultureMedia extends Product {
         }
     }
 
+    public enum QuantificationUnit {
+        ML(true),
+        L(true),
+        G(false),
+        KG(false);
+
+        private final boolean toRTUProduct;
+
+        QuantificationUnit(boolean toRTUProduct) {
+            this.toRTUProduct = toRTUProduct;
+        }
+
+        private boolean isToRTUProduct() {
+            return toRTUProduct;
+        }
+    }
+
     public CultureMediaFinality getFinality() {
         return finality;
     }
@@ -66,15 +151,27 @@ public class CultureMedia extends Product {
         return storageConditions;
     }
 
-    public boolean isReadyToUse() {
-        return readyToUse;
-    }
-
-    public PhysicalForm getPhysicalForm() {
-        return form;
+    public PhysicalUnit getPhysicalUnit() {
+        return physicalUnit;
     }
 
     public  boolean isProtectOfLight() {
         return protectOfLight;
+    }
+
+    public double getQtdPerUnit() {
+        return qtdPerUnit;
+    }
+
+    public QuantificationUnit getQuantityUnit() {
+        return quantificationUnit;
+    }
+
+    public double getFinalPhLevel() {
+        return finalPhLevel;
+    }
+
+    public double getPreparationGramsPerLiter() {
+        return preparationGramsPerLiter;
     }
 }
