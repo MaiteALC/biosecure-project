9fc4565b461b29fdbed4f3ea6030d0b115371dc6
MaiteALC <maitexp549@gmail.com>
2025-12-05 17:56:40 -0300

    test(product): created unit tests and builder for SampleContainer class

---
+++ apps/web-api-java/src/test/java/br/com/biosecure/utils/SampleContainerBuilder.java
@@ -0,0 +1,62 @@
+package br.com.biosecure.utils;
+
+import java.time.LocalDate;
+import br.com.biosecure.model.product.Product.PackagingType;
+import br.com.biosecure.model.product.SampleContainer;
+import br.com.biosecure.model.product.SampleContainer.*;
+
+public class SampleContainerBuilder {
+    // General attributes of products
+    private String name = "Sanitizer Test";
+    private double price = 54.90;
+    private String manufacturer = "Test Manufacturer";
+    private String batchNumber = "Batch-1A";
+    private LocalDate expirationDate = LocalDate.of(2027, 3, 25);
+    private PackagingType packagingType = PackagingType.BOX;
+    private double quantityPerPackage = 30.0;
+
+    // Specific attributes of Sample Container
+    private ClosingMethod closingMethod = ClosingMethod.CELLULOSE_STOPPER;
+    private SterilizationMethod sterilizationMethod = SterilizationMethod.ETHYLENE_OXIDE;
+    private Material material = Material.BOROSILICATE_GLASS;
+    private double capacity = 1;
+
+    private static class SampleContainerDummy extends SampleContainer {
+        public SampleContainerDummy(String name, double price, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, double quantityPerPackage, SterilizationMethod sterilizationMethod, ClosingMethod closingMethod, Material materialType, double capacityML) {
+        
+            super(name, price, manufacturer, batchNumber, expirationDate, packagingType, quantityPerPackage, sterilizationMethod, closingMethod, materialType, capacityML);
+        }
+    }
+
+    public SampleContainerBuilder withClosingMethod(ClosingMethod closingMethod) {
+        this.closingMethod = closingMethod;
+
+        return this;
+    }
+
+    public SampleContainerBuilder withSterilizationMethod(SterilizationMethod sterilizationMethod) {
+        this.sterilizationMethod = sterilizationMethod;
+
+        return this;
+    }
+
+    public SampleContainerBuilder withMaterial(Material material) {
+        this.material = material;
+
+        return this;
+    }
+
+    public SampleContainerBuilder withCapacity(double capacity) {
+        this.capacity = capacity;
+
+        return this;
+    }
+
+    public static SampleContainerBuilder aSampleContainer() {
+        return new SampleContainerBuilder();
+    }
+    
+    public SampleContainer build() {
+        return new SampleContainerDummy(name, price, manufacturer, batchNumber, expirationDate, packagingType, quantityPerPackage, sterilizationMethod, closingMethod, material, capacity);
+    }
+}
