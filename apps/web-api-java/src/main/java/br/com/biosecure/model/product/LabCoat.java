bb0c476f40c5299b976282793f795c9f5aacf58f
MaiteALC <maitexp549@gmail.com>
2025-12-16 12:49:24 -0300

    refactor(LabCoat): simplify number validations using NumberUtils class and implement notification pattern
    Changes:
    - Validate numerical attributes using methods of NumberUtils class to simplify the logic in class constructor.
    - Add new fabric type (MIX_COTTON_POLYESTER).
    - Create 2 new attributes for FabricType enum 'disposable' (used to set 'isDisposble' attribute of superclass PPE) and commercial name.
    - Rename FabricTypeInstance (SMS_NON_VOWEL -> POLYPROPYLEN)
    - Create unit tests to cover edge cases and expected scenarios.

--- apps/web-api-java/src/main/java/br/com/biosecure/model/product/LabCoat.java
+++ apps/web-api-java/src/main/java/br/com/biosecure/model/product/LabCoat.java
@@ -1,6 +1,8 @@
 package br.com.biosecure.model.product;
 
 import java.time.LocalDate;
+import br.com.biosecure.utils.NotificationContext;
+import br.com.biosecure.utils.NumberUtils;
 
 public class LabCoat extends PPE {
     private final FabricType fabricType;
@@ -10,7 +12,15 @@ public class LabCoat extends PPE {
 
     public LabCoat(String name, double price, String manufacturer, String batchNumber, LocalDate expirationDate, PackagingType packagingType, int quantityPerPackage, Size size, String certificateOfApproval, FabricType fabricType, int grammage, CuffStyle cuffStyle, CollarType collarType) {
        
-        super(name, price, manufacturer, batchNumber, expirationDate, packagingType, quantityPerPackage, size, certificateOfApproval, fabricType == FabricType.SMS_NON_WOVEN ? true : false /* <-- is disposable?*/);
+        super(name, price, manufacturer, batchNumber, expirationDate, packagingType, quantityPerPackage, size, certificateOfApproval, fabricType.isDisposableFabric());
+
+        NotificationContext notification = new NotificationContext();
+
+        NumberUtils.validateNumericalAttribute(grammage, 20, "grammage (g/cm²)", 350, notification);
+
+        if (notification.hasErrors()) {
+            throw new InvalidProductAttributeException(notification.getErrors());
+        }
 
         this.fabricType = fabricType;
         this.grammage = grammage;
@@ -19,9 +29,26 @@ public class LabCoat extends PPE {
     }
 
     public enum FabricType {
-        COTTON_100,
-        POLYESTER,
-        SMS_NON_WOVEN
+        COTTON_100("Cotton", false),
+        POLYESTER("Polyester", false),
+        POLYPROPYLEN("SPP", true),
+        MIX_COTTON_POLYESTER("Cotton and polyester", false);
+
+        private String commercialName;
+        private boolean disposable;
+
+        FabricType(String commercialName, boolean disposable) {
+            this.commercialName = commercialName;
+            this.disposable = disposable;
+        }
+
+        public String getCommercialName() {
+            return commercialName;
+        }
+
+        public boolean isDisposableFabric() {
+            return disposable;
+        }
     }
 
     public enum CuffStyle {
